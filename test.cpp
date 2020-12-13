41986806fcd431cec1e35f2408a533a3f00fe980
darnellkikoo <darnellkikoo@gmail.com>
2020-12-13 13:34:44 +0700

    push 2

--- test.cpp
+++ test.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main()
 {
+    int b;
+    scanf("%d", &b);
     int a = 8;
-    printf("%d\n", a+1);
+    for(int i=1; i<= a; i++)
+    {
+        printf("%d\n", i);
+    }
     return 0;
 }
